7d4b354a622ba7369a4250a8e68593b752493c76
Konstantinos Lamogiannis <kostaslamo91@gmail.com>
2021-11-08 18:07:47 +0200

    add: function that posts a new visit

---
+++ services/visit.js
@@ -0,0 +1,26 @@
+const Visit = require('../models/Visit');
+
+/**
+ * Save Visit to Database
+ *
+ * @function postVisit
+ * @param {Object} data
+ * @param {String} data.url The tier.app/*** URL
+ * @param {Boolean} data.valid If url matched with a posted one
+ * @param {String} data.ip The IP of the visitor
+ * @param {Object} data.metadata Other metadata
+ * @return {Promise}
+ */
+const postVisit = (data) =>
+  new Promise((resolve, reject) => {
+    const { url, valid, ip, metadata } = data;
+    const visit = new Visit({ url, valid, ip, metadata });
+    visit
+      .save()
+      .then((dbVisit) => resolve(dbVisit))
+      .catch((err) => reject(err));
+  });
+
+module.exports = {
+  postVisit,
+};
