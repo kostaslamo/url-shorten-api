c380d7cb88e5486d62376d88a6ee86aa999157a4
Konstantinos Lamogiannis <kostaslamo91@gmail.com>
2021-11-08 13:23:54 +0200

    add: logger middleware

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,11 @@ const checkHealth = require('./checkHealth');
 
 const router = express.Router();
 
-router.use('/checkHealth', checkHealth);
+const requestLoggerMiddleware = (req, res, next) => {
+  global.LOGGER.info(`REQ.${req.method}@${req.originalUrl}`);
+  next();
+};
+
+router.use('/checkHealth', requestLoggerMiddleware, checkHealth);
 
 module.exports = router;
